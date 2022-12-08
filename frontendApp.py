1a1132ca9241e8bfbe43e8fb53fd1514b5ab3b97
Imisi Fakunle <sc20if@leeds.ac.uk>
2022-12-08 15:39:28 +0000

    flashing messages

--- frontendApp.py
+++ frontendApp.py
@@ -10,32 +10,51 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-df = pd.read_csv('test.csv')
+df = pd.read_csv('data_bar.csv')
+questions = ['Largest % Plastic waste in stream', '2nd Smallest % Inadequately managed waste', 'Smallest % Littered waste', 'Largest Waste Generation (kg/day)', '2nd Smallest Plastic waste generation (kg/day)', 'Smallest Inadequately managed plastic waste (kg/day)', '2nd Largest Plastic waste littered (kg/day)', 'Largest Mismanaged plastic waste (kg/pp)', 'Smallest Mismanaged plastic waste in 2010 (tonnes)', '3rd Largest Mismanaged plastic waste in 2025 (tonnes)']
+cols = ['% Plastic waste in stream', '% Inadequately managed waste', '% Littered waste', 'Waste Generation (kg/day)', 'Plastic waste generation (kg/day)', 'Inadequately managed plastic waste (kg/day)', 'Plastic waste littered (kg/day)', 'Mismanaged plastic waste (kg/pp)', 'Mismanaged plastic waste in 2010 (tonnes)', 'Mismanaged plastic waste in 2025 (tonnes)']
+def choose_index():
+    yes = 1
+    selected = []
+    index = r.randint(0, len(cols)-1)
+    for x in selected:
+        if x != index:
+                yes = 1
+        else:
+                yes = 0
+    if yes == 1:
+        selected.append(index)
+    return index
+
 
 def build_banner(df):
     return html.Div(
-        id="banner",
+        className="banner",
         children=[
             html.H5("Plastic Pollution in different Countries"),
-            html.H6("Geographical Map vs Bar Chart"),
+            html.H6("Geographical Map vs Bar Chart")
         ],
     )
-questions = ['this1?', 'this2']
-index = 0
+
+
 def define_question(index):
     return questions[index]
-
+def make_bar(index):
+    fig_bar = px.bar(df, x=cols[index], y='Country')
+    return fig_bar
 def build_QandA():
+    index = choose_index()
     return html.Div(
-                id="button options",
+                className="button options",
                 children=[
-                    html.H6(questions[index]),
+                    dcc.Graph(figure=make_bar(index)),
+                    html.H6(define_question(index)),
                     html.A(
                         html.Button(children="ENTERPRISE DEMO"),
                         href="https://plotly.com/get-demo/",
                     ),
                     html.Button(
-                        id="learn-more-button", children="LEARN MORE", n_clicks=0
+                        children="LEARN MORE", n_clicks=0
                     )
                 ]
     )
@@ -90,17 +109,16 @@ def place_buttons():
         html.H4("Pollution")])
 
 '''
-fig_bar = px.bar(df.head(15), x='Country', y='Pollution')
+#fig_bar1 = px.bar(df, x='Country', y=cols[index])
+#fig_bar2 = px.bar(df, x='Country', y=cols[index])
 server = app.server
 
-def increment_index(index):
-    index += 1
 
 app.layout = html.Div([
     build_banner(df),
-    dcc.Graph(figure=fig_bar),
-    build_QandA(),
-    increment_index(index)
+    #dcc.Graph(figure=fig_bar1),
+    build_QandA()
+    #dcc.Graph(figure=fig_bar2)
     #generate_bar_chart(df),
     #treemap_develop(df),
 ])
